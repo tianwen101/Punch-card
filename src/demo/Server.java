ca5644cfe445db93a3d643da6b04d45a770da051
天 天 <1716061578@qq.com>
2019-05-17 12:51:25 +0800

    jdbc的数据更新修改

--- src/demo/Server.java
+++ src/demo/Server.java
@@ -7,9 +7,9 @@ import java.net.Socket;
 public class Server {
     public static void main(String[] args)throws IOException {
         //创建服务器对象，映射端口10086
-        ServerSocket ss = new ServerSocket(10086);
+        ServerSocket ss = new ServerSocket(8080);
         System.out.println("服务器启动，端口号：" + ss.getLocalPort());
-        while (true){
+        while (true){           //永真循环
             //获得客户端对象连接
             Socket socket = ss.accept();
             System.out.println("客户端：" + socket.getInetAddress() + "上线了");
