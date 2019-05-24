9649b105c1f9172f21767e6db412ed3c650976a7
天 天 <1716061578@qq.com>
2019-05-24 12:42:04 +0800

    设计模式5

---
+++ src/demo2/Logger.java
@@ -0,0 +1,5 @@
+package demo2;
+
+public interface Logger {
+    void log(String message);
+}
---
+++ src/demo2/LoggerCloud.java
@@ -0,0 +1,8 @@
+package demo2;
+
+public class LoggerCloud implements Logger {
+    @Override
+    public void log(String message) {
+        System.out.println("日志输出到云平台" + message);
+    }
+}
